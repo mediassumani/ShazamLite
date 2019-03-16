4dffc24a71b6d4509cdad8fb4605da45d67c66b2
Medi Assumani <mediassumani49@gmail.com>
2019-03-16 13:04:19 -0400

    initial commit

---
+++ Shazam/Shazam/Shazam.h
@@ -0,0 +1,19 @@
+//
+//  Shazam.h
+//  Shazam
+//
+//  Created by Medi Assumani on 3/16/19.
+//  Copyright © 2019 Medi Assumani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shazam.
+FOUNDATION_EXPORT double ShazamVersionNumber;
+
+//! Project version string for Shazam.
+FOUNDATION_EXPORT const unsigned char ShazamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shazam/PublicHeader.h>
+
+
